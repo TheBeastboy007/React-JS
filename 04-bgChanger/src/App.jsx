84aa2124402231f7482a443dddbe49b651e1f8a4
TheBeastboy007 <hardikvaghela3003@gmail.com>
2025-06-12 18:10:10 +0530

    Day - 03 - bgChanger

---
+++ 04-bgChanger/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+function App() {
+  const [color, setColor] = useState("red")
+
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          {["blue", "green", "yellow", "pink", "orange", "purple", "black", "white"].map((clr) => (
+            <button
+              key={clr}
+              onClick={() => setColor(clr)}
+              className='outline-none px-4 py-1 rounded-full text-white shadow-lg capitalize'
+              style={{ backgroundColor: clr, color: clr === "yellow" || clr === "white" ? "black" : "white" }}
+            >
+              {clr}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
